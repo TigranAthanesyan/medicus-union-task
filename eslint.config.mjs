4d64a521aa2124975c9c8e9c330ce0eaaa98c8fd
Tigran_Atanesyan <tigran.atanesyan@picsart.com>
2025-07-19 00:47:21 +0400

    minor fixes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,6 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
-import prettier from "eslint-plugin-prettier";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,17 +10,9 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     ignores: [".next/**", "node_modules/**", "out/**", "dist/**"],
-  },
-  {
-    plugins: {
-      prettier,
-    },
-    rules: {
-      "prettier/prettier": "error",
-    },
   },
 ];
 
